c856f8e603e2721c599b6880fbe34e21dff90ece
mark-well <lacemerto05@gmail.com>
2026-07-09 19:26:15 +0800

    ref: organize the files better'

---
+++ src/inference.py
@@ -0,0 +1,25 @@
+import torch
+from src.siamese_network import SiamseNetwork
+from PIL import Image
+from src.transforms import test_transform
+import asyncio
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = SiamseNetwork().to(device)
+model.load_state_dict(torch.load("models/siamese_resnet18.pth", map_location=device))
+model.eval()
+
+def load_image(image: Image.Image):
+    image = image.convert("RGB")
+    image = test_transform(image)
+    image = image.unsqueeze(0)
+
+    return image
+
+def get_embedding(image: Image.Image):
+    img = load_image(image)
+    with torch.no_grad():
+        return model.get_embedding(img.to(device))
+    
+async def get_embedding_async(image: Image.Image):
+    return await asyncio.to_thread(get_embedding, image)
---
+++ src/siamese_network.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+import torchvision.models as models
+
+#Model
+resnet = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+
+class SiamseNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.encoder = SiameseEncoder()
+
+    def forward(self, image1, image2):
+        embedding1 = self.encoder(image1)
+        embedding2 = self.encoder(image2)
+
+        return embedding1, embedding2
+    
+    def get_embedding(self, image):
+        return self.encoder(image)
+    
+class SiameseEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        #Resnet as the feature extractor
+        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
+        self.embedding = nn.Linear(512, 128)
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = x.view(x.size(0), -1)
+        x = self.embedding(x)
+
+        #Normalize the embedding
+        x = nn.functional.normalize(x, p=2, dim=1)
+        return x
